111d85bafbb2e097eaf516df00c07b96e5fbb3e0
Jonas Lima <Jonaslimadomingues@gmail.com>
2025-05-21 14:57:31 -0300

    Project created

---
+++ src/test/java/com/beposuperior/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beposuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
